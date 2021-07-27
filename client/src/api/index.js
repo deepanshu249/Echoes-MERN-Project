6309349aac76f15126a3a240d63f9f0d477efbf4
deepanshu249 <67109622+deepanshu249@users.noreply.github.com>
2021-07-27 17:11:44 +0530

    Add files via upload

---
+++ client/src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// axios instance
+// change this after deploying backend and before production build
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;      
+    }
+    
+    return req;  
+});
+
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
